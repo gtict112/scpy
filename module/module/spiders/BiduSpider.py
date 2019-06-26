156bcbbac351b2cbf5a89549effe945aa2e39d98
wangzongbiao <wangzongbiao@9nius.com>
2019-06-26 22:13:29 +0800

    2

--- module/module/spiders/BiduSpider.py
+++ module/module/spiders/BiduSpider.py
@@ -8,4 +8,8 @@ class BiduSpider(scrapy.Spider):
     start_urls = ['https://6034.vip/member/index#']
 
     def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = '%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
         pass
